61570b65c3782a2f057a7c4080cecdd1deaec0e0
elonuyts <emma.nuyts@ugent.be>
2022-05-29 16:55:43 +0200

    updated report

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,20 +4,26 @@ import { Button } from "react-bootstrap";
 export class Home extends Component {
   render() {
     return (
-      <div style={{marginLeft:"200px", marginTop:"100px"}}>
-        <p >This compliance checker is a deliverable from a master thesis at Ghent University.</p>
-        <p>You can upload a Linked Building Data file and check the compliance with the Belgian building code on accessibility.</p>
-        <p>The tool uses the Shapes Constraint Languages (SHACL) to validate building projects,
-        using the '
-          <a href= 'https://github.com/zazuko/rdf-validate-shacl'>rdf-validate-shacl</a>', '
-          <a href="https://github.com/rdf-ext/rdf-ext">rdf-ext</a>' and '
-          <a href="https://github.com/rdfjs/N3.js">RDF.js</a>' modules.
+      <div style={{ marginLeft: "200px", marginTop: "100px" }}>
+        <p>
+          This compliance checker is a deliverable from a master thesis at Ghent
+          University.
+        </p>
+        <p>
+          You can upload a Linked Building Data file and check the compliance
+          with the Belgian building code on accessibility.
+        </p>
+        <p>
+          The tool uses the Shapes Constraint Languages (SHACL) to validate
+          building projects, using '
+          <a href="https://github.com/RDFLib/pySHACL">pySHACL</a>', and '
+          <a href="https://nodejs.org/api/child_process.html">child_process</a>
+          '.
         </p>
         <br></br>
         <Button variant="outline-dark" href="/upload">
           Validate a building project
         </Button>
-        
       </div>
     );
   }
